47da30cb80e6e3df534ffc1ad4dc3d69059aba08
Mohamad Kholid Bughowi <kholidbughowi@gmail.com>
2024-07-03 18:37:59 +0700

    feat: add all dotfiles

---
+++ .config/nvim/lua/config/options.lua
@@ -0,0 +1,14 @@
+-- Options are automatically loaded before lazy.nvim startup
+-- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+-- Add any additional options here
+vim.o.termguicolors = true
+vim.filetype.add({
+  extension = {
+    mdx = "mdx",
+  },
+})
+vim.opt.cursorlineopt = "number"
+vim.o.background = "dark"
+vim.o.timeoutlen = 5000 -- Timoutlen to delay which key
+vim.g.autoformat = false
+vim.g.netrw_banner = 0
---
+++ .config/nvim/lua/plugins/disabled.lua
@@ -0,0 +1,35 @@
+return {
+  -- coding
+  { "hrsh7th/cmp-path", enabled = false },
+  { "hrsh7th/cmp-buffer", enabled = false },
+  { "garymjr/nvim-snippets", enabled = false },
+  { "rafamadriz/friendly-snippets", enabled = false },
+  { "echasnovski/mini.pairs", enabled = false },
+  { "echasnovski/mini.ai", enabled = false },
+
+  -- colorscheme
+  { "catppuccin/nvim", enabled = false },
+  { "folke/tokyonight.nvim", enabled = false },
+
+  -- editor
+  { "nvim-neo-tree/neo-tree.nvim", enabled = false },
+  { "nvim-pack/nvim-spectre", enabled = false },
+  { "folke/flash.nvim", enabled = false },
+  { "folke/todo-comments.nvim", enabled = false },
+
+  -- linting
+  { "mfussenegger/nvim-lint", enabled = false },
+
+  -- treesitter
+  { "nvim-treesitter/nvim-treesitter-textobjects", enabled = false },
+  { "windwp/nvim-ts-autotag", enabled = false },
+
+  -- ui
+  { "rcarriga/nvim-notify", enabled = false },
+  { "folke/noice.nvim", enabled = false },
+  { "MunifTanjim/nui.nvim", enabled = false },
+  { "nvimdev/dashboard-nvim", enabled = false },
+
+  -- util
+  { "folke/persistence.nvim", enabled = false },
+}
