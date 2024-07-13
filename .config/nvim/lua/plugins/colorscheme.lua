899ae46fd0d31d564a21e3d754e2b024ca132ee7
Mohamad Kholid Bughowi <kholidbughowi@gmail.com>
2024-07-13 20:55:46 +0700

    feat(neovim): add obsidian plugin

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -6,6 +6,17 @@ return {
     priority = 1000,
   },
   {
+    "ellisonleao/gruvbox.nvim",
+    priority = 1000,
+    config = true,
+    opts = {
+      contrast = "hard",
+      palette_overrides = {
+        dark0_hard = "#0e0c12",
+      },
+    },
+  },
+  {
     "LazyVim/LazyVim",
     opts = {
       colorscheme = "moonfly",
