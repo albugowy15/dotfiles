9c668819911f3f34e755c3aa26d449954d12eba1
Mohamad Kholid Bughowi <kholidbughowi@gmail.com>
2024-09-14 11:45:35 +0700

    fix(neovim): disable cache for cyberdream colorscheme

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -9,24 +9,11 @@ return {
       hide_fillchars = false,
       borderless_telescope = false,
       terminal_colors = true,
-      cache = true, -- generate cache with :CyberdreamBuildCache and clear with :CyberdreamClearCache
-
       theme = {
         variant = "dark",
       },
     },
   },
-  -- {
-  --   "ellisonleao/gruvbox.nvim",
-  --   priority = 1000,
-  --   config = true,
-  --   opts = {
-  --     contrast = "hard",
-  --     palette_overrides = {
-  --       dark0_hard = "#0e0c12",
-  --     },
-  --   },
-  -- },
   {
     "LazyVim/LazyVim",
     opts = {
