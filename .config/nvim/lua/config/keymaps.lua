0f058f77a793672d0f0ef537c3362e0e7c349f8a
Mohamad Kholid Bughowi <kholidbughowi@gmail.com>
2024-09-14 11:44:50 +0700

    feat(nvim): keep last yanked when pasting

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -2,3 +2,4 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 vim.keymap.set("n", "<leader>e", "<cmd>Explore<cr>", { desc = "Open netrw" })
+vim.keymap.set("v", "p", '"_dP', { desc = "Keep last yanked when pasting" })
--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -1,24 +1,27 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
-vim.o.termguicolors = true
+vim.opt.colorcolumn = "100"
+vim.opt.updatetime = 50
+vim.opt.scrolloff = 8
+vim.opt.signcolumn = "yes"
+vim.opt.hlsearch = false
+vim.opt.termguicolors = true
+vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.undofile = true
+vim.opt.nu = true
 vim.filetype.add({
   extension = {
     mdx = "mdx",
   },
 })
--- vim.opt.cursorlineopt = "number"
-vim.o.background = "dark"
+vim.opt.cursorlineopt = "number"
+-- vim.o.background = "dark"
 vim.g.autoformat = false
-
--- Netrw config
-vim.g.netrw_banner = 0
 vim.g.netrw_winsize = 30
 vim.g.netrw_keepdir = 0
 vim.g.netrw_localcopydircmd = "cp -r"
-
--- LSP Server to use for Python.
--- Set to "basedpyright" to use basedpyright instead of pyright.
 vim.g.lazyvim_python_lsp = "pyright"
--- Set to "ruff_lsp" to use the old LSP implementation version.
 vim.g.lazyvim_python_ruff = "ruff"
+-- vim.g.root_spec = { "cwd" }
